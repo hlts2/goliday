68f47d70ac0307e9c0a7cd28340c7e1a3a9be058
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-01-12 23:16:45 +0900

    Refactor

--- goliday.go
+++ goliday.go
@@ -1,26 +1,25 @@
 package goliday
 
 import (
-	"fmt"
 	"time"
 )
 
 // IsHoliday returns true if date is holiday.
 func IsHoliday(year, month, day int) bool {
-	return hds.containsKey(formatYmd(year, month, day))
+	return holidays.contains(formatYmd(year, month, day))
 }
 
 // IsHolidayTime returns true if t(time.Time) is holiday.
 func IsHolidayTime(t time.Time) bool {
-	return hds.containsKey(formatYmd(t.Year(), int(t.Month()), t.Day()))
-}
-
-func formatYmd(year, month, day int) string {
-	return fmt.Sprintf("%d-%d-%d", year, month, day)
+	return holidays.contains(formatYmd(t.Year(), int(t.Month()), t.Day()))
 }
 
 // InMonth --
-func InMonth(year, month int) {}
+func InMonth(year, month int) []Holiday {
+	return holidays.inMonth(year, month)
+}
 
 // InYear --
-func InYear(year int) {}
+func InYear(year int) []Holiday {
+	return holidays.inYear(year)
+}
