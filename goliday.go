c4dab9129b67855f7728c231f5964e39a46261ed
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-01-13 15:02:33 +0900

    Refactor

--- goliday.go
+++ goliday.go
@@ -2,20 +2,18 @@ package goliday
 
 // IsHoliday returns true if there is a holiday set as an option.
 func IsHoliday(ops ...Option) bool {
-	evalOp := newEvaluateOption()
-	for _, op := range ops {
-		op(evalOp)
-	}
-
-	return table.contains(evalOp)
+	return table.contains(evaluatedOption(ops...))
 }
 
 // Holidays returns Holiday slice that matches option values.
 func Holidays(ops ...Option) []Holiday {
+	return table.holidaysByEvaluateOption(evaluatedOption(ops...))
+}
+
+func evaluatedOption(ops ...Option) *evaluateOption {
 	evalOp := newEvaluateOption()
 	for _, op := range ops {
 		op(evalOp)
 	}
-
-	return table.holidaysByEvaluateOption(evalOp)
+	return evalOp
 }
